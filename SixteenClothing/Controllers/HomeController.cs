8b1cb7f6cf547b75ebfaeadfece3904412035ab2
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2021-12-21 02:17:07 +0400

    Works are done

---
+++ SixteenClothing/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SixteenClothing.Data;
+using SixteenClothing.Models;
+using SixteenClothing.ViewModel;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+
+namespace SixteenClothing.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+            VmHome model = new VmHome()
+            {
+                banners = _context.HomeBanners.ToList(),
+                products = _context.Products.ToList(),
+                settings = _context.Settings.FirstOrDefault()
+            };
+            return View(model);
+        }
+    }
+}
---
+++ SixteenClothing/ViewModel/VmHome.cs
@@ -0,0 +1,13 @@
+﻿using SixteenClothing.Models;
+using System.Collections.Generic;
+
+namespace SixteenClothing.ViewModel
+{
+    public class VmHome : VmSettings
+    {
+        public List<HomeBanners> banners { get; set; }
+
+        public List<Product> products { get; set; }
+
+    }
+}
